b0d04ac98ee02da69d3980f039d42ba31b5c8a0b
deploy niyon <deploy@niyon.com.br>
2020-08-19 15:31:34 -0300

    Removing ChatV1 SDK, deprecated by apple

--- ios/Classes/SwiftFlutterZendeskPlugin.swift
+++ ios/Classes/SwiftFlutterZendeskPlugin.swift
@@ -4,7 +4,6 @@ import SupportSDK
 import ChatSDK
 import ChatProvidersSDK
 import ZendeskCoreSDK
-import ZDCChat
 import MessagingSDK
 
 public class SwiftFlutterZendeskPlugin: NSObject, FlutterPlugin {
@@ -16,97 +15,78 @@ public class SwiftFlutterZendeskPlugin: NSObject, FlutterPlugin {
     }
     
     public func handle(_ call: FlutterMethodCall, result: @escaping FlutterResult) {
-        result("iOS handle:" + call.method)
-        
+
         switch call.method {
-        case "init":
-            Logger.isEnabled = true
-            Logger.defaultLevel = .verbose
-            guard let dic = call.arguments as? Dictionary<String, Any> else { return }
-            
-            let accountKey = dic["accountKey"] as? String ?? ""
-            let applicationId = dic["applicationId"] as? String ?? ""
-            let clientId = dic["clientId"] as? String ?? ""
-            let domainUrl = dic["domainUrl"] as? String ?? ""
-            let emailIdentifier = dic["emailIdentifier"] as? String ?? "emailIdentifier"
-            let nameIdentifier = dic["nameIdentifier"] as? String ?? "nameIdentifier"
-            
-            let phone = dic["phone"] as? String ?? ""
-            let email = dic["email"] as? String ?? ""
-            let name = dic["name"] as? String ?? ""
-            
-            Zendesk.initialize(appId: applicationId,
-                               clientId: clientId,
-                               zendeskUrl: domainUrl)
-            Support.initialize(withZendesk: Zendesk.instance)
-            Zendesk.instance?.setIdentity(Identity.createAnonymous(name:nameIdentifier, email: emailIdentifier))
-            
-            
-            //V1 Chat
-            ZDCChat.initialize(withAccountKey: accountKey)
-            ZDCChat.updateVisitor { user in
-                user?.phone = phone
-                user?.name = name
-                user?.email = email
-            }
-            //CHAT V2 SDK
-            Chat.initialize(accountKey: accountKey,appId:applicationId)
-            result("iOS init completed" )
-        case "startChatV1":
-            startChatV1()
-        case "startChatV2":
-            guard let dic = call.arguments as? Dictionary<String, Any> else { return }
-            let botLabel = dic["botLabel"] as? String ?? "Anwser Bot"
-            let phone = dic["phone"] as? String ?? ""
-            
-            let name = dic["name"] as? String ?? ""
-            do {
-                try startChatV2(botLabel: botLabel,phone: phone,name: name)
-            } catch let error{
-                print("error:\(error)")
-            }
-        case "helpCenter":
-            let currentVC = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            let hcConfig = HelpCenterUiConfiguration()
-            hcConfig.showContactOptions = true
-            let helpCenter = HelpCenterUi.buildHelpCenterOverviewUi(withConfigs: [hcConfig])
-            currentVC?.pushViewController(helpCenter, animated: true)
-            result("iOS helpCenter UI:" + helpCenter.description + "   ")
-        case "requestView":
-            let rootViewController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            let viewController = RequestUi.buildRequestUi(with: [])
-            rootViewController?.pushViewController(viewController, animated: true)
-        case "requestListView":
-            let rootViewController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            let viewController = RequestUi.buildRequestList(with: [])
-            rootViewController?.pushViewController(viewController, animated: true)
-        case "changeNavStatus":
-            let rootViewController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-            guard let dic = call.arguments as? Dictionary<String, Any> else { return }
-            
-            let isShow = dic["isShow"] as? Bool ?? false
-            rootViewController?.setNavigationBarHidden(!isShow, animated: false)
-            result("rootViewController?.isNavigationBarHidden = isShow >>>>>")
-        case "getPlatformVersion":
-            result("iOS X.X")
-            
-        default:
-            result("method not implemented")
+            case "init":
+                print("case init")
+                Logger.isEnabled = true
+                Logger.defaultLevel = .verbose
+                guard let dic = call.arguments as? Dictionary<String, Any> else { return }
+                print("case teste")
+
+                let accountKey = dic["accountKey"] as? String ?? ""
+                let applicationId = dic["applicationId"] as? String ?? ""
+                let clientId = dic["clientId"] as? String ?? ""
+                let domainUrl = dic["domainUrl"] as? String ?? ""
+
+                Zendesk.initialize(appId: applicationId,
+                                   clientId: clientId,
+                                   zendeskUrl: domainUrl)
+                Support.initialize(withZendesk: Zendesk.instance)
+                Zendesk.instance?.setIdentity(Identity.createAnonymous())
+
+
+        
+                
+                //CHAT V2 SDK
+                Chat.initialize(accountKey: accountKey,appId:applicationId)
+                result("iOS init completed" )
+            case "startChatV2":
+                guard let dic = call.arguments as? Dictionary<String, Any> else { return }
+                let botLabel = dic["botLabel"] as? String ?? "Anwser Bot"
+                let phone = dic["phone"] as? String ?? ""
+
+                let name = dic["name"] as? String ?? ""
+                do {
+                    try startChatV2(botLabel: botLabel,phone: phone,name: name)
+                } catch let error{
+                    print("error:\(error)")
+                }
+            case "helpCenter":
+                guard let dic = call.arguments as? Dictionary<String, Any> else { return }
+                let contactUsButtonVisible = dic["contactUsButtonVisible"] as? Bool ?? false
+
+                let currentVC = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
+                let hcConfig = HelpCenterUiConfiguration()
+                hcConfig.showContactOptions = contactUsButtonVisible
+
+                let articleUiConfig = ArticleUiConfiguration()
+                articleUiConfig.showContactOptions = contactUsButtonVisible
+
+                let helpCenter = HelpCenterUi.buildHelpCenterOverviewUi(withConfigs: [hcConfig, articleUiConfig])
+                currentVC?.pushViewController(helpCenter, animated: true)
+                result("iOS helpCenter UI:" + helpCenter.description + "   ")
+            case "requestView":
+                let rootViewController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
+                let viewController = RequestUi.buildRequestUi(with: [])
+                rootViewController?.pushViewController(viewController, animated: true)
+            case "requestListView":
+                let rootViewController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
+                let viewController = RequestUi.buildRequestList(with: [])
+                rootViewController?.pushViewController(viewController, animated: true)
+            case "changeNavStatus":
+                let rootViewController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
+                guard let dic = call.arguments as? Dictionary<String, Any> else { return }
+
+                let isShow = dic["isShow"] as? Bool ?? false
+                rootViewController?.setNavigationBarHidden(!isShow, animated: false)
+                result("rootViewController?.isNavigationBarHidden = isShow >>>>>")
+            case "getPlatformVersion":
+                result("iOS " + UIDevice.current.systemVersion + "v1")
+
+            default:
+                result("method not implemented")
         }
-    }
-    func startChatV1(){
-        //https://developer.zendesk.com/embeddables/docs/ios-chat-sdk/chat
-        
-        let navigationController = UIApplication.shared.keyWindow?.rootViewController as? UINavigationController
-        
-        ZDCChat.start(in: navigationController, withConfig: {config in
-            config?.preChatDataRequirements.name = .notRequired
-            config?.preChatDataRequirements.email = .notRequired
-            config?.preChatDataRequirements.phone = .requiredEditable
-        })
-        
-        // Hides the back button because we are in a tab controller
-        //        ZDCChat.instance().chatViewController.navigationItem.hidesBackButton = true
     }
     
     func startChatV2(botLabel:String,phone:String,name:String) throws {
